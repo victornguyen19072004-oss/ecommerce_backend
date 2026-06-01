800accc3ecce812a8430e6b7302d1875e2c5ea91
victornguyen19072004-oss <victornguyen19072004@gmail.com>
2026-06-01 14:49:15 +0700

    Add findByTags_TagName method to ProductRepository
    Added method to find products by tag name.

--- src/main/java/com/nguyendinhphuoccao/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.nguyendinhphuoccao.ecommerce.repository;
 
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.nguyendinhphuoccao.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     boolean existsBySlug(String slug);
+    // Tìm danh sách sản phẩm dựa vào tên Tag (Ví dụ: "NEW" hoặc "SALE")
+    List<Product> findByTags_TagName(String tagName);
 }
