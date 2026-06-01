4dd208281d430c671b04dc48c64be59420d3ff7c
victornguyen19072004-oss <victornguyen19072004@gmail.com>
2026-06-01 15:59:42 +0700

    Add missing import for List in ProductServiceImpl

--- src/main/java/com/nguyendinhphuoccao/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/service/impl/ProductServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
+import java.util.List; // BỔ SUNG DÒNG NÀY
 import java.util.Set;
 import java.util.UUID;
 
