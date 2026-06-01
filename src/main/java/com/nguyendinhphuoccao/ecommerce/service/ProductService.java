dbd5acd00b606f74751569eed9f358e449820af0
victornguyen19072004-oss <victornguyen19072004@gmail.com>
2026-06-01 14:49:40 +0700

    Add getProductsByTag method to ProductService
    Added a method to retrieve products by tag.

--- src/main/java/com/nguyendinhphuoccao/ecommerce/service/ProductService.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.nguyendinhphuoccao.ecommerce.service;
 
-import java.util.UUID;
-
 import com.nguyendinhphuoccao.ecommerce.dto.ProductRequest;
 import com.nguyendinhphuoccao.ecommerce.entity.Product;
+import java.util.List;
+import java.util.UUID;
 
 public interface ProductService {
     Product createProduct(ProductRequest request);
     Product updateProductTagsAndPrice(UUID productId, ProductRequest request);
+    List<Product> getProductsByTag(String tagName); // Thêm dòng này
 }
