9f5aa0777bca21110bcad60ad659147594f6d80e
victornguyen19072004-oss <victornguyen19072004@gmail.com>
2026-06-01 14:25:14 +0700

    Modified Product.java

--- src/main/java/com/nguyendinhphuoccao/ecommerce/entity/Product.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/entity/Product.java
@@ -1,33 +1,25 @@
 package com.nguyendinhphuoccao.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "products")
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false, unique = true)
     private String slug;
 
     @Column(name = "product_name", nullable = false)
@@ -46,8 +38,7 @@ public class Product {
     private BigDecimal buyingPrice;
 
     @Column(nullable = false)
-    @Builder.Default
-    private Integer quantity = 0;
+    private Integer quantity;
 
     @Column(name = "short_description", length = 165, nullable = false)
     private String shortDescription;
@@ -56,17 +47,17 @@ public class Product {
     private String productDescription;
 
     @Column(name = "product_type", length = 64)
-    private String productType; // 'simple' hoặc 'variable'
+    private String productType;
 
-    @Builder.Default
-    private Boolean published = false;
+    @Column(nullable = false)
+    private Boolean published;
 
-    // Ánh xạ bảng trung gian product_tags
-    @ManyToMany(fetch = FetchType.EAGER)
+    // Quan hệ Nhiều-Nhiều sinh ra bảng product_tags
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
-        name = "product_tags",
-        joinColumns = @JoinColumn(name = "product_id"),
-        inverseJoinColumns = @JoinColumn(name = "tag_id")
+            name = "product_tags",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
 }
