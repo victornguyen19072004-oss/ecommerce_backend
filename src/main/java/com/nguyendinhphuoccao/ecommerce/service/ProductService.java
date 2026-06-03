bd11db289b9e680d3582ac96d2e75da59113c554
Nguyễn Đình Phước Cao <victornguyen19072004@gmail.com>
2026-06-03 09:56:59 +0700

    Add getProductsByCategory method to ProductService
    Added a new method to retrieve products by category.

--- src/main/java/com/nguyendinhphuoccao/ecommerce/service/ProductService.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/service/ProductService.java
@@ -1,12 +1,17 @@
 package com.nguyendinhphuoccao.ecommerce.service;
 
-import com.nguyendinhphuoccao.ecommerce.dto.ProductRequest;
-import com.nguyendinhphuoccao.ecommerce.entity.Product;
 import java.util.List;
 import java.util.UUID;
+
+import com.nguyendinhphuoccao.ecommerce.dto.ProductRequest;
+import com.nguyendinhphuoccao.ecommerce.entity.Product;
 
 public interface ProductService {
     Product createProduct(ProductRequest request);
     Product updateProductTagsAndPrice(UUID productId, ProductRequest request);
+
     List<Product> getProductsByTag(String tagName); // Thêm dòng này
+    
+    // BỔ SUNG DÒNG NÀY
+    List<Product> getProductsByCategory(String categoryName);
 }
